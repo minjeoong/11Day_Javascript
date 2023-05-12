8315d006fe2dfa3644768967e7b14801f686c085
minjeoong <orange_mj@naver.com>
2023-05-12 23:09:53 +0900

    수정

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # JavaScript_Basic
-11일차 JavaScript 기본 세션입니다.
+
+makeIntro(name, mbti) :
+form에 입력한 이름과 MBTI 문자열이 주어졌을 때,
+이름과 Mbti 를 포함해서 문자열로 return.
+
+
+assign2(arr, divisor) :
+
+arr는 정수가 담겨있는 배열
+divisor은 양의 정수
+arr의 각 원소 중 divisor로 나누어 떨어지지 않는 값들을 배열에 담아 return.
+단, 이때 반환하는 배열은 오름차순으로 정렬해서 return.
+만약 나누어 떨어지지 않는 값이 단 한개도 없다면 배열에 "없어요!" 만 담아서 return.
+
--- assign.js
+++ assign.js
@@ -7,20 +7,17 @@ export const makeIntro = (name, mbti) => {
 
 // 과제 2
 export const assign2 = (arr, divisor) => {
-  // array의 각 element 중 divisor로 나누어 떨어지지 않는 수를 담은 배열을 return하세요.
-  // 반환하는 배열은 오름차순으로 정렬해야 합니다.
-  // 나누어 떨어지지 않는 값이 한개도 없다면 배열에 "없어요!" 만 담아서 return 합니다.
 
-  // 여기에 코드를 작성하세요.
   let answer = [];
   arr.forEach((element) => {
-    if((element % divisor) != 0){
-      answer.push(element);
+    if((element % divisor) != 0){ //0으로 나누어지지 않는 수라면
+      answer.push(element); //answer에 넣기
     }
   });
-  if(answer.length === 0) {
+  if(answer.length === 0) {//길이가 0이라면. => 나누어 떨어지는 값이 한개도 없다면.
     answer.push("없어요!");
   }
-  answer.sort();
+  answer.sort(); //오름차순 정렬
   return answer;
+  
 };
